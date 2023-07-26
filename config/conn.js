02e610e20ad53b5466e13efb72fd1b6dc45e9a41
Masood Abbas <masoodabbas421@gmail.com>
2023-07-26 17:34:58 +0500

    update

--- config/conn.js
+++ config/conn.js
@@ -1,9 +1,5 @@
 const mongoose=require(`mongoose`)
 const DB=`mongodb+srv://masoodabbas:allah4mine@cluster0.nzci0so.mongodb.net/registration`
-<<<<<<< HEAD
-=======
-
->>>>>>> d3e48c881a2151f8fb043b90a195a5943791e2bc
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
