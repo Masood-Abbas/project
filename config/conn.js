d3e48c881a2151f8fb043b90a195a5943791e2bc
Masood Abbas <85512438+Masood-Abbas@users.noreply.github.com>
2023-07-26 17:30:32 +0500

    Update conn.js

--- config/conn.js
+++ config/conn.js
@@ -1,6 +1,7 @@
 const mongoose=require(`mongoose`)
+const DB=`mongodb+srv://masoodabbas:allah4mine@cluster0.nzci0so.mongodb.net/registration`
 
-mongoose.connect(`mongodb://127.0.0.1:27017/registration`,{
+mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
