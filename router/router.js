4fb0163c7693a199198babe4b2bf01d34d1db7c2
ali-rafique-pikessoft <ali.rafique@pikessoft.com>
2023-07-27 11:49:54 +0500

    Login Api

--- router/router.js
+++ router/router.js
@@ -8,17 +8,20 @@ router.post("/login",async(req,res)=>{
     const username=req.body.username
     const password=req.body.password
     const user=await Register.findOne({username})
-
-    const token=await user.generateAuthToken()
-    console.log(`the success part`+token);
-    if(user.password===password){
-        // res.send(user)
-        res.send("success")
-    }else{
-        res.send("invalid detail")
-    }
+   
+    if (!user) {
+        res.status(404).send('User not found');
+      } else {
+        if (user.password === password) {
+          const token = await user.generateAuthToken();
+          const data = { token };
+          res.send(data);
+        } else {
+          res.status(404).send('User not found');
+        }
+      }
 } catch (error) {
-    res.status(404).send(`invalid login`)
+    res.status(404).send(error)
 }
 })
 
