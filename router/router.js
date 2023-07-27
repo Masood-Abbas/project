cce5ab77100f53d10636f095f63cb4daa53db3a3
Masood Abbas <masoodabbas421@gmail.com>
2023-07-27 13:26:09 +0500

    upate

--- router/router.js
+++ router/router.js
@@ -1,7 +1,40 @@
 const express=require(`express`)
 const Register=require(`../models/register`)
+const user=require(`../models/user`)
 
 const router=express.Router()
+router.post("/",async(req,res)=>{
+try {
+      const newUser=new user({
+        employe_no:req.body.employe_no,
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        email:req.body.email,
+        employe_type:req.body.employe_type,
+        category:req.body.category
+    })
+    const result=await newUser.save()
+   
+    // const token=await user.generateAuthToken()
+    // console.log(`the success part`+token);
+} catch (error) {
+  res.status(404).send(`invalid details`) 
+}
+})
+
+// router.post("/login",async(req,res)=>{
+//     try {
+//      const email=req.body.email
+//     //  const password=req.body.password
+//      const usera=await user.findOne({email})
+//      res.send(usera)
+//     }
+//     catch (error) {
+//         res.status(404).send(`invalid login`)
+//         console.log(error)
+//     }
+// })
+
 // router.post("/",async(req,res)=>{
 //     const newUser=new Register(req.body)
 //    const result=await newUser.save()
@@ -15,14 +48,17 @@ router.post("/login",async(req,res)=>{
     const password=req.body.password
     const user=await Register.findOne({username})
 
-    const token=await user.generateAuthToken()
-    console.log(`the success part`+token);
-    if(user.password===password){
-        // res.send(user)
-        res.send("success")
-    }else{
-        res.send("invalid detail")
-    }
+    if (!user) {
+        res.status(404).send('User not found');
+      } else {
+        if (user.password === password) {
+          const token = await user.generateAuthToken();
+          const data = { token };
+          res.send(data);
+        } else {
+          res.status(404).send('User not found');
+        }
+      }
 } catch (error) {
     res.status(404).send(`invalid login`)
 }
