675a8820ab8755f6552722307b493419b533751c
Masood Abbas <masoodabbas421@gmail.com>
2023-07-28 18:16:11 +0500

    add api

--- router/router.js
+++ router/router.js
@@ -1,9 +1,10 @@
 const express=require(`express`)
 const Register=require(`../models/register`)
 const user=require(`../models/user`)
+const bcrypt=require(`bcrypt`)
 
 const router=express.Router()
-router.post("/",async(req,res)=>{
+router.post("/registration",async(req,res)=>{
 try {
       const newUser=new user({
         employe_no:req.body.employe_no,
@@ -11,29 +12,44 @@ try {
         last_name:req.body.last_name,
         email:req.body.email,
         employe_type:req.body.employe_type,
-        category:req.body.category
+        category:req.body.category,
+        password:req.body.password
     })
     const result=await newUser.save()
-   
-    // const token=await user.generateAuthToken()
-    // console.log(`the success part`+token);
+    const token = await newUser.generateAuthToken();
+    const data = { token };
+    res.send(data);
 } catch (error) {
   res.status(404).send(`invalid details`) 
+  console.log(error)
 }
 })
 
-// router.post("/login",async(req,res)=>{
-//     try {
-//      const email=req.body.email
-//     //  const password=req.body.password
-//      const usera=await user.findOne({email})
-//      res.send(usera)
-//     }
-//     catch (error) {
-//         res.status(404).send(`invalid login`)
-//         console.log(error)
-//     }
-// })
+router.post("/login",async(req,res)=>{
+    try {
+     const email=req.body.email
+     const password=req.body.password
+     const usera=await user.findOne({email})
+     if (!usera) {
+      res.status(404).send('User not found');
+      } else {
+        const isMatch=await bcrypt.compare(password,usera.password)
+        if (isMatch) {
+          const token = await usera.generateAuthToken();
+          const data = { token };
+          res.send(data);
+          console.log(usera);
+        }else {
+            res.status(404).send("invalid password")
+     res.send(usera)
+    }
+  }
+}
+    catch (error) {
+        res.status(404).send(`invalid login`)
+        console.log(error)
+    }
+})
 
 // router.post("/",async(req,res)=>{
 //     const newUser=new Register(req.body)
