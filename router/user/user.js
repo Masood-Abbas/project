f383d80cf4ae626f4bdc493f5e0028369596117a
Masood Abbas <masoodabbas421@gmail.com>
2023-09-09 15:27:09 +0500

    separate file of curd api of user

---
+++ router/user/user.js
@@ -0,0 +1,149 @@
+const express=require(`express`)
+const Register=require(`../../models/register`)
+const user=require(`../../models/user`)
+const bcrypt=require(`bcrypt`)
+const User=require('../../models/user')
+const nodemailer = require('nodemailer');
+
+const router=express.Router()
+
+
+
+
+// Register user
+router.post("/",async(req,res)=>{
+try {
+    const {employeeNo,firstName,lastName,email,employeeType,category,password,profileImg}=req.body
+    if(user.employeeNo===employeeNo){
+      res.json({msg:"employeeNo is present"})
+    }else{
+      const newUser=new user({
+        employeeNo,firstName,lastName,email,employeeType,category,password,profileImg
+    })
+    const result=await newUser.save()
+    const token = await newUser.generateAuthToken();
+    const data = { token };
+
+    // Email
+     // Create a Nodemailer transporter using SMTP
+     const transporter=nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+          user:"a86094305@gmail.com",
+          pass:"ebuppjsrpobpfadt"
+        }
+      })
+      // Email options
+      const mailOptions={
+        from:"a86094305@gmail.com",
+        to: email,
+        subject:"information",
+        text:`Employe No: ${employeeNo}
+              First Name: ${firstName}
+              Last Name: ${lastName}
+              Email: ${email}
+              Employe-type: ${employeType}
+              Catgory: ${category}
+              password: ${password}
+              Profile_img: ${profileImg}`
+      }
+      // send email
+      transporter.sendMail(mailOptions,(error,info)=>{
+        if (error) {
+          return res.status(500).send(`error occure sending mail`)
+        } else {
+          console.log('Email sent: ' + info.response);
+           res.status(201).send(`Email send successly` )
+        }
+      })
+    res.send(data);}
+} catch (error) {
+  if(error.code===11000){
+    return res.send("user are present")
+  }
+  res.status(404).send(`plz entaer valid detail`) 
+  console.log(error)
+}
+})
+
+router.post("/",async(req,res)=>{
+    try {
+     const {email,password}=req.body
+     const usera=await user.findOne({email})
+     if (!usera) {
+      res.status(404).send('User not found');
+      } else {
+        const isMatch=await bcrypt.compare(password,usera.password)
+        if (isMatch) {
+          const token = await usera.generateAuthToken();
+          const data = { token };
+          res.send(usera);
+          console.log(usera);
+        }else {
+            res.status(404).send("invalid password")
+     res.send(usera)
+    }
+  }
+}
+    catch (error) {
+        res.status(404).send(`invalid login`)
+        console.log(error)
+    }
+})
+// update user
+
+router.patch(`/`,async(req,res)=>{
+  
+
+try {
+  const employeeNo=req.body.employeeNo
+  const updateUser=await user.findOneAndUpdate({employeeNo},req.body,{
+    new:true
+  })
+  if(!updateUser){
+    res.status(404).send('User not found');
+  }
+  res.send(updateUser)
+} catch (error) {
+  res.status(500).send(`invalid`)
+  console.log(error)
+}
+})
+
+
+router.get('/', async (req, res) => {
+    try {
+      const users = await User?.find({}, 'firstName lastName category email employeeNo password id');
+    
+      if (users?.length) {
+        res.json(users);
+      } else {
+        res.status(404).send('users not found');
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching users', error: error.message });
+    }
+  });
+
+
+//Admin login 
+router.post("/login",async(req,res)=>{
+   try {
+    const username=req.body.username
+    const password=req.body.password
+    const user=await Register.findOne({username})
+
+    const token=await user.generateAuthToken()
+    console.log(`the success part`+token);
+    if(user.password===password){
+        // res.send(user)
+        res.send("success")
+    }else{
+        res.send("invalid detail")
+    }
+} catch (error) {
+    res.status(404).send(error)
+}
+})
+
+module.exports=router;
